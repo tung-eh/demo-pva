b35754a21d172266fc52383fd98c541081085372
Tung Teng <thetungctn@gmail.com>
2019-10-03 15:18:22 +0700

    Remove default layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Ubuntu:300,400,500,700"],
+        },
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
